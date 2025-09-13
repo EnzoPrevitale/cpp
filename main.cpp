83ffde00801d2f98ec7747030557046e24865feb
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-09-13 01:14:29 -0300

    Orientação a Objetos

--- main.cpp
+++ main.cpp
@@ -3,24 +3,46 @@
 using namespace std;
 
 class Person{
+    private:
+        float salary;
+
     public:
         string name;
         int age;
+
+        Person(string name, int age, float salary) {
+            this->name = name;
+            this->age = age;
+            this->salary = salary;
+        }
         
         void greet() {
-            cout << "Hello, my name is " << name << ". I am " << age << " years old.";
+            cout << "Hello, my name is " << name << ". I am " << age << " years old." << endl;
         }
+
+        friend void displaySalary(Person p);
 };
 
+void displaySalary(Person p) {
+    cout << "Salary: " << p.salary;
+}
+
 int main() {
-    Person person;
-    
+    string name;
+    int age;
+    float salary;
+
     cout << "Type your name: ";
-    getline(cin, person.name);
+    getline(cin, name);
     cout << "Type your age: ";
-    cin >> person.age;
-    
+    cin >> age;
+    cout << "Type your salary: ";
+    cin >> salary;
+
+    Person person(name, age, salary);
     person.greet();
+
+    displaySalary(person);
     
     return 0;
 }
